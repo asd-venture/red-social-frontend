427c5973fbbd37b9a3f868ee9ab248735ed2951c
Edrey De Aza <edreydeaza1515@gmail.com>
2023-01-08 10:19:18 -0400

    v1.2.13

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useAuth0 } from '@auth0/auth0-react'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Index from './pages'
+import Index from './pages/Index'
 import Home from './pages/Home'
 import Load from './components/Load'
 import Profile from './pages/Profile'
