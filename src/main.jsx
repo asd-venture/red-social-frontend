bd79aab41088ebc32a3560625c333e3831379d7c
Edrey De Aza <edreydeaza1515@gmail.com>
2023-01-08 17:54:27 -0400

    v1.2.15

--- src/main.jsx
+++ src/main.jsx
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Auth0Provider 
         domain={domain} 
         clientId={clientId} 
-        redirectUri={window.location.origin+'/home'}
+        redirectUri={window.location.origin}
       >
         <App />
       </Auth0Provider>
